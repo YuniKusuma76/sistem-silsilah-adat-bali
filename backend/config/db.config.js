8a3ef3b56272818b07271b81c53752e275a4da97
PLAYER 212 <yunikusumayanti02@gmail.com>
2026-07-27 23:31:43 +0800

    penyesuaian database

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -3,11 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const db = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS, {
-    host: process.env.DB_HOST,
+const db = new Sequelize(process.env.DATABASE_URL, {
     dialect: "postgres",
     logging: false,
     pool: {
@@ -15,6 +11,12 @@ const db = new Sequelize(
       min: 0,
       acquire: 30000,
       idle: 10000
+    },
+    dialectOptions: {
+      ssl: process.env.NODE_ENV === "production" ? {
+        require: true,
+        rejectUnauthorized: false
+      } : false
     }
   }
 );
